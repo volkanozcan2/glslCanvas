54f96a59c43b8c2d0922f7a79018870d9529e222
v8o6@ya.ru <vozcan86@googlemail.com>
2018-10-07 22:01:00 +0300

    edits

--- src.js
+++ src.js
@@ -2,16 +2,12 @@ $(document).ready(function() {
 
 
     const shader = `
-// Author: uvefan Guuvavson
-// Title: Worley noise 2x2x2
 
 #ifdef GL_ES
 precision lowp float;
 #endif
-#define pi 3.1415
 uniform vec2 u_resolution;
 uniform float u_time;
-#define iTime u_time;
 
 float circle(in vec2 _st, in float _radius){
     vec2 dist = _st-vec2(0.5);
